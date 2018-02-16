35b435dff893bb94546aa6c678dcc244c95342d1
BuildTools <unconfigured@null.spigotmc.org>
2018-02-16 14:01:59 +0300

    Edited MSSQL Backup script

--- Interactive-FindUser/README.md
+++ Interactive-FindUser/README.md
@@ -4,18 +4,11 @@
 
 Import-Module .\Interactive-FindUser.psm1
 
-<<<<<<< HEAD:Interactive-FindUser/README.md
-# Start dynamic user search
-# You don't need to enter a full name,
-# just type a part of it and hit "Enter"
-# then function retuns you full name
-=======
 * Start dynamic user search
 * You don't need to enter a full name,
 * just type a part of it and hit "Enter"
 * then function retuns you full name
 
->>>>>>> 43a0145e49688391f02d143255d10b5010a5fa48:README.md
 $userName = Interactive-UserFind -Users_OU $UserOU
 
 pause
--- MSSQL-Encrypted-Backup/MSSQL-Encrypted-Backup.ps1
+++ MSSQL-Encrypted-Backup/MSSQL-Encrypted-Backup.ps1
@@ -1,30 +1,30 @@
 ﻿#region Define variables
-$date            = Get-Date -Format dd.MMM
+$date            = Get-Date -Format dd.MM.yyyy
 $retentionPeriod = 6
 
 $DBName          = "<DBNAME>"
 $remoteStorage   = "<REMOTE_STORAGE_PATH>"
 $fileName        = "<FILE_NAME>"
-# NOTE: generate certificate using "Create-Certificate.sql"
+# NOTE: generate encryptor using "Create-Certificate.sql"
 $encryptorName   = "<ENCRYPTOR_NAME>"
 
 $localBackupFolder = "<LOCAL_BACKUP_FOLDER>"
-$localBackupFile   = $localBackupFolder+$fileName+$date+".bak"
+$localBackupFile   = $localBackupFolder+$fileName+"_"+$date+".bak"
 
 # mail settings
 $from       = "<FROM_EMAIL>" 
-$to         = "TO_EMAIL"
-$message    = "MESSAGE_TEXT"
+$to         = "<TO_EMAIL>"
+$message    = "<MESSAGE_TEXT>"
 $mailServer = "<SMTP_SERVER>"
 #endregion
 
 # Create encrypted backup
 $encryptionOption = New-SqlBackupEncryptionOption -Algorithm Aes256 -EncryptorType ServerCertificate -EncryptorName $encryptorName
-Backup-SqlDatabase -ServerInstance localhost -Database $DBName -BackupFile $localPath -EncryptionOption $encryptionOption
+Backup-SqlDatabase -ServerInstance localhost -Database $DBName -BackupFile $localBackupFile -EncryptionOption $encryptionOption
 
 # Copy backup file to remote storage
 New-PSDrive -Name BCK -PSProvider FileSystem -Root $remoteStorage
-Copy-Item $localPath -Destination BCK:\
+Copy-Item $localBackupFile -Destination BCK:\
 
 # Remove Old DB backup
 $selection = ls BCK: | where {$_.Name -clike $fileName}
@@ -37,8 +37,18 @@ foreach($file in $selection)
     }
 }
 
-Remove-Item $localPath
-Remove-PSDrive BCK
+if(Test-Path $($remoteStorage+$fileName+"_"+$date+".bak"))
+{
+    Remove-Item $localBackupFile
+    Remove-PSDrive BCK
 
-Send-MailMessage -SmtpServer $mailServer -From $from `
-                 -To $to -Subject $message
+    Send-MailMessage -SmtpServer $mailServer -From $from `
+                     -To $to -Subject $message
+}
+else
+{
+    Remove-PSDrive BCK
+
+    Send-MailMessage -SmtpServer $mailServer -From $from `
+                     -To $to -Subject "$DBName Backup FAILED!" -Priority High
+}
