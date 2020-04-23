2d52c9a3bd1201bbc790c725867bff1465386aca
Strakh Alex <strakh.alex@gmail.com>
2020-04-23 20:33:19 +0300

    Add CatsAndDogs.ps1 script

---
+++ CatsAndDogs.ps1
@@ -0,0 +1,30 @@
+[CmdletBinding()]
+
+param(
+    [string]$ContentPath = "C:\DogsAndCats"
+)
+
+if(!(Test-Path $ContentPath)) {
+    Write-Error("> Path was not found!")
+    Exit(1)
+}
+
+$Substitutes = @{
+    "Злая" = "Добрая"; 
+    "кошка" = "собака";
+    "мяу" = "гав"
+}
+
+$ContentItems = Get-ChildItem -Path $ContentPath -Recurse -Include "*.txt","*.log"
+
+foreach($ContentItem in $ContentItems) {
+    $Content = Get-Content $ContentItem -Encoding UTF8
+    foreach($Key in $Substitutes.Keys) {
+        if($Content.Contains($Key)) {
+            Write-Verbose "Replace $Key with $($Substitutes.$Key) in file $ContentItem"
+            $Content = $Content -replace $Key,$Substitutes.$Key
+        }
+    }
+
+    Set-Content -Path $ContentItem.FullName -Value $Content -Encoding UTF8 -Force
+}
